e1a64e5e84ff3d4ff71983a11134a6fb97ef02ed
saif ansari <kisandeveloper2@gmail.com>
2026-08-08 18:02:27 +0530

    fix: Vercel ESM require error for firebase-admin

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["tejomargjob.com", "www.tejomargjob.com", "localhost:3045"],
+  serverExternalPackages: ["firebase-admin", "mongoose"],
 };
 
 export default nextConfig;
