c22cd3e13884c7e4b2517e88ff3d8cf996c449d5
saif ansari <kisandeveloper2@gmail.com>
2026-08-08 18:07:27 +0530

    fix: extend serverExternalPackages for jose and jwks-rsa

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ["tejomargjob.com", "www.tejomargjob.com", "localhost:3045"],
-  serverExternalPackages: ["firebase-admin", "mongoose"],
+  serverExternalPackages: ["firebase-admin", "mongoose", "jwks-rsa", "jose"],
 };
 
 export default nextConfig;
